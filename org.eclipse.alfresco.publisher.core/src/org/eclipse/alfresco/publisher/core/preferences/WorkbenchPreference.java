de1404a3c99d9458256843cb2e745c6f2c79728b
olivier.nouguier <olivier.nouguier@gmail.com>
2011-12-22 22:38:00 +0100

    Organize properties

---
+++ org.eclipse.alfresco.publisher.core/src/org/eclipse/alfresco/publisher/core/preferences/WorkbenchPreference.java
@@ -0,0 +1,31 @@
+package org.eclipse.alfresco.publisher.core.preferences;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.IWorkbenchPropertyPage;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+public class WorkbenchPreference extends PropertyPage implements
+		IWorkbenchPropertyPage {
+
+	public WorkbenchPreference() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		composite.setLayout(layout);
+		GridData data = new GridData(GridData.FILL);
+		data.grabExcessHorizontalSpace = true;
+		composite.setLayoutData(data);
+
+		
+		return composite;
+	}
+
+}
